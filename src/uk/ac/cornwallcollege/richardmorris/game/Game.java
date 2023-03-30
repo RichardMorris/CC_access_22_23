6fdb174b1c78caf9f8ae65530ad0353bd78e636a
Richard Morris <rich@singsurf.org>
2023-03-30 11:12:52 +0100

    Allow different characters to be selected
    In TeamRocket the toString methods is overridden to give a custom message.
    In Game a scanner is created, and different types selected depending on user input.

--- src/uk/ac/cornwallcollege/richardmorris/game/Game.java
+++ src/uk/ac/cornwallcollege/richardmorris/game/Game.java
@@ -1,17 +1,38 @@
 package uk.ac.cornwallcollege.richardmorris.game;
 
+import java.util.Scanner;
+
+/**
+ * A simple main method for the game, allows the creation of various types of character
+ */
 public class Game {
 
     public static void main(String[] args) {
-
-        Character c1 = new Character(10,5,4,1,2,"Zorro");
-        System.out.println(c1.toString());
-        Character c2 = new PlayableCharacter(5,6,7,8,9,"Safdari");
-        System.out.println(c2);
-        Character c3 = new NPC(1,2,3,4,5,"Joe Hill");
-        System.out.println(c3);
-        Character c4 = new TeamRocket(1,2,3,4,5,"Rockets");
-        System.out.println(c4);
-
+        // sets up the input
+        Scanner scan = new Scanner(System.in);
+        // prints welcome message
+        System.out.println("Please select first character\n1 for a Playable Character\n2 for a NPC\n3 for Team Rocket");
+        // Reads a number from the console
+        int choice = scan.nextInt();
+        // Variable used to store the character,
+        // note how we use the super-class variable here
+        Character c1;
+        // Take different paths depending on choice variable
+        switch (choice) {
+            case 1:
+                c1 = new PlayableCharacter(5,6,7,8,9,"Safdari");
+                break;
+            case 2:
+                c1 = new NPC(1,2,3,4,5,"Joe Hill");
+                break;
+            case 3:
+                c1 = new TeamRocket(1,2,3,4,5,"Rockets");
+                break;
+            default: // this branch is taken if none of the other branches are followed
+                c1 = new NPC(1,2,3,4,5,"Joe Hill");
+        }
+        // print out what we have created
+        // it will use the overridden toString methods
+        System.out.println("The first character is "+c1);
     }
 }
--- src/uk/ac/cornwallcollege/richardmorris/game/TeamRocket.java
+++ src/uk/ac/cornwallcollege/richardmorris/game/TeamRocket.java
@@ -6,6 +6,15 @@ public class TeamRocket extends  NPC {
         super(health, attack, defence, speed, size, name);
     }
 
+    /**
+     * A customises toString method
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "Go TeamRocket";
+    }
+
     @Override
     public void dealDamage(Character c, int amount) {
         c.health -= attack;
